6866e14ba70480892b6f3dde6d7ca568d080ff31
Nikolay Bayborodin <nbbrdn@gmail.com>
2024-04-30 21:39:20 +1000

    Add type hints

--- openai_canvas/apps/local_time.py
+++ openai_canvas/apps/local_time.py
@@ -2,46 +2,49 @@ import json
 import os
 import time
 from datetime import datetime
-from typing import Union
+from typing import Any, Dict, List, Tuple, Union
 
 import openai
 import requests
+from openai.types.beta.assistant import Assistant
+from openai.types.beta.thread import Thread
+from openai.types.beta.threads.run import Run
 
-INSTRUCTIONS = """
+INSTRUCTIONS: str = """
 You're a support service chat bot.
 Use the provided functions to answer questions.
 Synthesise answer based on provided function output and be consise.
 """
 
+api_key: str = os.environ.get("OPENAI_API_KEY")
+client: openai.Client = openai.OpenAI(api_key=api_key)
 
-def fetch_local_time(offset) -> Union[datetime, str]:
+
+# Fetch local time function
+def fetch_local_time(offset: int) -> Union[datetime, str]:
     print("fetching data from remote...")
-    url = "https://flow.bayborodin.ru/time/"
-    params = {"offset": offset}
+    url: str = "https://flow.bayborodin.ru/time/"
+    params: Dict[str, Any] = {"offset": offset}
     response = requests.get(url, params=params, timeout=5)
-    status_code = response.status_code
-    if status_code == 200:
-        dt_str = response.json()["current_time"]
-        datetime_object = datetime.fromisoformat(dt_str)
+    if response.status_code == 200:
+        current_time: str = response.json()["current_time"]
+        datetime_object: datetime = datetime.fromisoformat(current_time)
         print(f"remote response: {datetime_object}")
         return datetime_object
-    return f"Error: Unable to fetch local time. Status code: {status_code}"
+    return (
+        "Error: Unable to fetch local time. "
+        "Status code: {response.status_code}"
+    )
 
 
 available_functions = {
     "fetch_local_time": fetch_local_time,
 }
 
-api_key = os.environ.get("OPENAI_API_KEY")
-client = openai.OpenAI(api_key=api_key)
 
-available_functions = {
-    "fetch_local_time": fetch_local_time,
-}
-
-
-def execute(offset):
-    tools = [
+def run_conversation() -> None:
+    # Assistant setup
+    tools: List[Dict[str, Any]] = [
         {
             "type": "function",
             "function": {
@@ -61,84 +64,87 @@ def execute(offset):
         }
     ]
 
-    assistant = client.beta.assistants.create(
+    assistant: Assistant = client.beta.assistants.create(
         name="Support chat bot",
         instructions=INSTRUCTIONS,
         model="gpt-4-turbo",
         tools=tools,
     )
 
-    query = input()
-    run, thread = create_message_and_run(assistant=assistant, query=query)
-    print(run)
-
+    # Conversation loop
     while True:
-        run = client.beta.threads.runs.retrieve(
-            thread_id=thread.id, run_id=run.id
-        )
-        print("run status", run.status)
+        query: str = input()
+        run, thread = create_message_and_run(assistant=assistant, query=query)
+        print(run)
 
-        if run.status == "requires_action":
-
-            function_name, arguments, function_id = get_function_details(run)
-
-            function_response = execute_function_call(function_name, arguments)
-
-            run = submit_tool_outputs(
-                run, thread, function_id, function_response
+        while True:
+            run: openai.Run = client.beta.threads.runs.retrieve(
+                thread_id=thread.id, run_id=run.id
             )
+            print("run status", run.status)
 
-            continue
-        if run.status == "completed":
+            if run.status == "requires_action":
+                function_name, arguments, function_id = get_function_details(
+                    run
+                )
+                function_response = execute_function_call(
+                    function_name, arguments
+                )
+                run = submit_tool_outputs(
+                    run, thread, function_id, function_response
+                )
+                continue
 
-            messages = client.beta.threads.messages.list(thread_id=thread.id)
-            latest_message = messages.data[0]
-            text = latest_message.content[0].text.value
-            print(text)
+            if run.status == "completed":
+                latest_message = client.beta.threads.messages.list(
+                    thread_id=thread.id
+                ).data[0]
+                print(latest_message.content[0].text.value)
+                user_input = input()
+                if user_input == "STOP":
+                    break
+                run, thread = create_message_and_run(
+                    assistant=assistant, query=user_input, thread=thread
+                )
+                continue
 
-            user_input = input()
-            if user_input == "STOP":
-                break
-
-            run, thread = create_message_and_run(
-                assistant=assistant, query=user_input, thread=thread
-            )
-
-            continue
-        time.sleep(1)
+            time.sleep(1)
 
 
-def create_message_and_run(assistant, query, thread=None):
+# Helper functions
+
+
+def create_message_and_run(
+    assistant: Assistant, query: str, thread: Thread = None
+) -> Tuple[Run, Thread]:
     if not thread:
         thread = client.beta.threads.create()
 
     client.beta.threads.messages.create(
         thread_id=thread.id, role="user", content=query
     )
-
-    run = client.beta.threads.runs.create(
+    run: Run = client.beta.threads.runs.create(
         thread_id=thread.id, assistant_id=assistant.id
     )
     return run, thread
 
 
-def get_function_details(run):
+def get_function_details(run: Run):
     print("\nrun.required_action\n", run.required_action)
 
-    function_name = run.required_action.submit_tool_outputs.tool_calls[
-        0
-    ].function.name
-    arguments = run.required_action.submit_tool_outputs.tool_calls[
-        0
-    ].function.arguments
-    function_id = run.required_action.submit_tool_outputs.tool_calls[0].id
+    called_function = run.required_action.submit_tool_outputs.tool_calls[0]
+    function_name = called_function.function.name
+    arguments = called_function.function.arguments
+    function_id = called_function.id
 
     print(f"function_name: {function_name} and arguments: {arguments}")
 
     return function_name, arguments, function_id
 
 
-def execute_function_call(function_name, arguments):
+def execute_function_call(
+    function_name: str, arguments: str
+) -> Union[str, datetime]:
     function = available_functions.get(function_name, None)
     if function:
         arguments = json.loads(arguments)
@@ -146,7 +152,12 @@ def execute_function_call(function_name, arguments):
     return f"Error: function {function_name} does not exist"
 
 
-def submit_tool_outputs(run, thread, function_id, function_response):
+def submit_tool_outputs(
+    run: Run,
+    thread: Thread,
+    function_id: str,
+    function_response: Union[str, datetime],
+) -> Run:
     run = client.beta.threads.runs.submit_tool_outputs(
         thread_id=thread.id,
         run_id=run.id,
--- openai_canvas/scripts/local_time.py
+++ openai_canvas/scripts/local_time.py
@@ -1,18 +1,8 @@
-import sys
-
 from openai_canvas.apps import local_time
 
 
 def main():
-    if len(sys.argv) == 2:
-        try:
-            offset = int(sys.argv[1])
-            local_time.execute(offset)
-        except ValueError:
-            print("UTC offset must be a number.")
-    else:
-        print("Usage: local-time <offset>")
-        sys.exit(1)
+    local_time.run_conversation()
 
 
 if __name__ == "__main__":
